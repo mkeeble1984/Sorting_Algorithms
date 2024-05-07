7522f6904d0dcf674d99569c1987286ceb535b38
mkeeble1984 <167688490+mkeeble1984@users.noreply.github.com>
2024-05-07 01:03:58 +0100

    Need to understand sorting algotrithm

--- Copy_Sort.py
+++ Copy_Sort.py
@@ -1,12 +1,14 @@
 def copy_sort(array):
+    # creates a copy of the original array called 'copy'
     copy = array[:]
+    # creates a new, blank array 'sorted_copy' to have elements added in
     sorted_copy = []
-    # Algorithm sequence to be added here.
 
+    # while there are elements in the array 'copy'
     while len(copy) > 0:
         minimum = 0
         for element in range(0, len(copy)):
-            if copy[element] < copy[minimum]:
+            if copy[element] < copy[minimum]:  # NEED TO UNDERSTAND WHAT IS HAPPENEING HERE
                 minimum = element
         print('\tRemoving', copy[minimum], 'from', copy)
         sorted_copy.append(copy.pop(minimum))
@@ -14,10 +16,14 @@ def copy_sort(array):
     return sorted_copy
 
 
+# Adds data into the array
 array = [5, 3, 1, 2, 6, 4]
+
+# Displays on the screen Title and original array
 print('Copy Sort...')
 print('Array :', array)
 
+# Runs the function 'copy_sort' using original array
 print('Copy :', copy_sort(array))
 print('Array :', array)
 
